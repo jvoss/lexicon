067b29932af3721f8fd737c50f283c37b4ad1e9e
Jonathan P. Voss <jvoss@onvox.net>
2013-08-16 12:42:59 -0400

    Updated base spec

--- spec/base_spec.rb
+++ spec/base_spec.rb
@@ -18,6 +18,16 @@ module Lexicon
         FileUtils.rm_r(@tmpdir)
       end
 
+      it 'should raise if not initialized' do
+        expect{Base.directory}.to raise_error Lexicon::NotInitialized
+      end
+
+      it 'should return the status of whether it is initialized or not' do
+        Lexicon::Base.init?.should be false
+        Lexicon::Base.init(:directory => @tmpdir)
+        Lexicon::Base.init?.should be true
+      end
+
       it 'should load a configuration from a YAML file' do
         lexicon = Lexicon::Base.load_yaml('spec/mocks/base.config.yaml')
         lexicon.directory.should == '/tmp/lexicon'
@@ -25,8 +35,6 @@ module Lexicon
       end
 
       it 'should initialize a new Lexicon::Base object' do
-        lexicon_class = Lexicon::Base
-
         lexicon = Lexicon::Base.init(:directory => @tmpdir)
         lexicon.init?.should be true
         Lexicon.const_defined?(:Log).should be true
@@ -35,6 +43,10 @@ module Lexicon
 
       it 'should raise if the directory argument is missing' do
         expect{Base.init({})}.to raise_error Lexicon::ArgumentError
+      end
+
+      it 'should return the base directory' do
+        Lexicon::Base.directory.should == @tmpdir
       end
 
     end # describe 'Base'
