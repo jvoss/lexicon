dd6c13c40f50c31ebdd3774693722034220f954a
Jonathan P. Voss <jvoss@onvox.net>
2013-08-20 16:54:57 -0400

    Cleaned up spec_sources to allow multiple RSpec runs without the need to flushall Redis entries

--- spec/sources_spec.rb
+++ spec/sources_spec.rb
@@ -12,19 +12,24 @@ module Lexicon
         Lexicon::Base.init(@@base_options)
       end
 
+      before :each do
+        # Clean up from previous RSpec run if needed
+        Base.delete_source(Base.source_by_name('test')) if Base.source_by_name('test')
+      end
+
       it 'should require a name during initialization' do
         expect{Source.new()}.to raise_error Lexicon::ArgumentError
       end
 
       it 'should not require a description during initialization' do
-        Source.new(:name => 'test3').description.should be nil
+        Source.new(:name => 'test').description.should be nil
       end
 
       it 'should register Lexicon::Base as an observer of a Source object' do
         #double_base = double(Lexicon::Base.init(:directory => '/tmp'))
         #double_base.should_receive(:update).once
-        Source.new(:name => 'test4')
-        Lexicon::Base.source_by_name('test4').is_a?(Source).should be true
+        Source.new(:name => 'test')
+        Lexicon::Base.source_by_name('test').is_a?(Source).should be true
       end
 
     end # describe 'Sources'
